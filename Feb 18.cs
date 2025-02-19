d90369bc9c7bf903c9e429ceab25b602f6fe51b7
Priyank <priyanklnu2@gmail.com>
2025-02-19 06:51:11 -0700

    Create Feb 18.cs

---
+++ Feb 18.cs
@@ -0,0 +1,9 @@
+//I learned how to create name for characters and some few other codes
+string firstName;
+char userOption;
+
+int gameScore;
+
+decimal particlesPerMillion;
+
+bool processedCustomer;
