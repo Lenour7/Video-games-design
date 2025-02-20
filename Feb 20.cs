9119a36b04c358be71accb3914f33ba4ffe10d36
Priyank <priyanklnu2@gmail.com>
2025-02-20 10:43:43 -0700

    Create Feb 20.cs

---
+++ Feb 20.cs
@@ -0,0 +1,10 @@
+//today I learned the stringcode for making names
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
