65398d82f36c0ff6282fd68cc8ce92f0a4ada154
Priyank <priyanklnu2@gmail.com>
2025-03-06 10:25:58 -0700

    Create Mar_6.cs

---
+++ Mar_6.cs
@@ -0,0 +1,9 @@
+//Today I learned how to add comments. In the example, it says to write comment with one quotation but then there is a limit to the characters. So I diecided to use double quotation so this way there will not be a limit to how many characters I use
+Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("Hello World!");
