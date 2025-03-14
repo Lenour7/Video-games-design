b11c2464c610a183ce5f4e66cf41dffc31b08287
Priyank <priyanklnu2@gmail.com>
2025-03-14 08:49:51 -0600

    Create Mar_14.cs

---
+++ Mar_14.cs
@@ -0,0 +1,5 @@
+//Today I learned how to combine strings using string interpolation
+//Modeule 4 units 3 and 4
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+//outcome should be C:\Output\First-Project\Data
