a24b3bc1b4be5c9912e28be240c3d86e89349dc8
Priyank <priyanklnu2@gmail.com>
2025-04-10 10:13:34 -0600

    Create Apr 10.cs

---
+++ Apr 10.cs
@@ -0,0 +1,4 @@
+//Today I completed unit 1 and 2 for module 5, I got too ahead but I think I should be fine because I learned how to add the number on cs
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
